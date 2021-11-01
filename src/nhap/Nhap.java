20948d8b93bbf6409a0e5aa9648b84994b865881
ChristDrumb0 <nhantn996@gmail.com>
2021-11-01 17:13:56 +0700

    upload_bai_tap_mang

--- src/nhap/Nhap.java
+++ src/nhap/Nhap.java
@@ -1,18 +1,18 @@
 package nhap;
 
+import java.util.Scanner;
+
 public class Nhap {
-    public static void main(String[] args) {
-        int i = 1;
-        while(i < 5){
-            if(i % 3 != 0){
-                i++;
-                continue;
-            }
-            System.out.println("Hello");
-            i++;
-        }
-
+    final static String ones[] = {" ", " ONE", " TWO", " THREE", " FOUR", " FIVE", " SIX", " SEVEN", " EIGHT", " NINE", " TEN", " ELEVEN", " TWELVE", " THIRTEEN", " FOURTEEN", " FIFTEEN", " SIXTEEN", " SEVENTEEN", " EIGHTEEN", " NINETEEN"};
+    final static String twos[] = {" ", " ", " TWENTY", " THIRTY", " FORTY", " FIFTY", " SIXTY", " SEVENTY", " EIGHTY", " NINETY"};
+    private static void numberToWord(int num, String val) {
+        System.out.print(num > 19 ? twos[num / 10] + " " + ones[num % 10] : ones[num]);
+        System.out.print(num > 0 ? val : "");
     }
-
-
+    public static void main(String[] args) {
+        System.out.print("Input a number:");
+        int num = new Scanner(System.in).nextInt();
+        numberToWord(((num / 100) % 10), " HUNDRED AND");
+        numberToWord((num % 100), " ");
+    }
 }
